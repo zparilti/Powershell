e948a909e3c9c1b34e1c87346738066caff8ef62
zparilti <67382808+zparilti@users.noreply.github.com>
2024-10-19 22:58:59 -0400

    Add files via upload

---
+++ 02-AddContenttoafile.ps1
@@ -0,0 +1,26 @@
+﻿# ------------------------------------------------------------------------
+# Author: Zeki
+# Topic: Powershell: Add-content cmdlet
+# Type: DOCUMENT AS CODE 
+# ------------------------------------------------------------------------
+
+clear; # clears powershell screen
+Set-Location c:\yourdirectory goes here; # set your working directory
+pwd # see current working directory
+
+
+# The `Add-Content` cmdlet appends content to a file. 
+# Type the content in the command or specify an object; e.g `Get-Date`.
+
+# Example 1: 
+# Adding command history, the current date and a string to a text file
+
+Add-Content -Path .\commandhistory.txt -Value (Get-History);
+Add-Content -Path .\commandhistory.txt -Value (Get-Date);
+Add-Content -Path .\commandhistory.txt -Value 'END OF FILE'; 
+   
+# Display the commandhistory.txt file content in powershell
+
+Get-Content -Path .\*.txt 
+
+### END OF FILE
