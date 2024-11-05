8dd1ddd4a2378636c7197fa71795c12d83f3c838
zparilti <67382808+zparilti@users.noreply.github.com>
2024-11-05 08:11:58 -0500

    Add files via upload

---
+++ Modules.ps1
@@ -0,0 +1,26 @@
+﻿clear;
+
+# Powershell Modules and Commands, e.g.;
+
+#Module: Microsoft.PowerShell.Core(Imported)
+# Command: Clear-History
+
+# GETTING MODULE: 
+
+Get-Module Microsoft.PowerShell.Management # gets the modules
+Show-Command # see all modules and commands
+
+Get-Service | Where-Object {$_.Status -eq "Running"}
+Get-Service | Where-Object {$_.Status -eq "Stopped"}
+
+Start-Job -ScriptBlock {Get-Process -Name Powershell}
+
+Get-Help .\07-Objects-KeyWord.ps1
+
+Get-Module -ListAvailable | ForEach-Object -MemberName Path;
+Get-Module -ListAvailable | ForEach-Object -MemberName Name;
+Get-Module -ListAvailable | ForEach-Object Get-Member
+Get-Module -ListAvailable | Foreach Path
+
+sleep 1
+
