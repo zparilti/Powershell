863e75778c524afdc72640c52e5581aa75c28732
zparilti <67382808+zparilti@users.noreply.github.com>
2024-11-06 13:55:21 -0500

    Add files via upload

---
+++ Get-Verb.ps1
@@ -0,0 +1,29 @@
+﻿# Get approved Powershell verbs
+clear
+get-help -category function -name get*
+sleep 5
+
+clear
+
+$(Get-Verb | Get-Member)
+sleep 15
+
+
+Get-Verb
+
+sleep 3
+
+Get-Verb un*
+
+sleep 3
+
+Get-Verb | Where-Object Group # see all verbs 
+#PUT EXAMPLES HERE
+
+
+Get-Verb | Where-Object Group -EQ Security
+
+sleep 3
+
+Get-Command -Module Microsoft.Powershell.Utility | Where-Object Verb -NotIn (Get-Verb).Verb
+
