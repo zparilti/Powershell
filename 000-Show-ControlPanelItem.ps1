921e9052de55ef05d56b6d122ce62931005ec395
zparilti <67382808+zparilti@users.noreply.github.com>
2024-11-10 12:33:04 -0500

    Add files via upload

---
+++ 000-Show-ControlPanelItem.ps1
@@ -0,0 +1,6 @@
+﻿#Show-ControlPanelItem 
+
+# Show-ControlPanelItem -Name "AutoPlay"
+# Show-ControlPanelItem -Name System
+# Show-ControlPanelItem -Name U* # Items with starting U letters
+Show-ControlPanelItem -Name D*
