52d428a5b3ea97349b2259d56945226a5d2958ab
zparilti <67382808+zparilti@users.noreply.github.com>
2024-11-12 13:19:27 -0500

    Add files via upload

---
+++ 0000-0-~.Object.ps1
@@ -0,0 +1,32 @@
+﻿#Get-Help *-Object # lists related help
+
+# Get-Command -Type Cmdlet | Sort-Object -Property Noun | Format-Table -GroupBy Noun
+
+clear
+
+# Get-Help compare -Examples
+# ________________________________
+
+# 1. Compare the content of two txt files
+# Compare-Object -ReferenceObject (Get-Content -Path .\i.txt) -DifferenceObject (Get-Content -Path .\s.txt)
+
+# As function
+
+function compare_Object {
+
+    $reference_Object = Get-Content -Path .\i.txt
+    $difference_Object = Get-Content -Path .\s.txt
+
+    Compare-Object -ReferenceObject $reference_Object -DifferenceObject $difference_Object
+
+}
+
+compare_Object
+
+
+# 2. Compare each line of content and exclude the differences
+
+
+
+
+# Select-String
