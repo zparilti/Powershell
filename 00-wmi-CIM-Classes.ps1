2e9c6b9f52692338b89c5fd1a7450b343cc1e37b
zparilti <67382808+zparilti@users.noreply.github.com>
2024-11-21 10:45:17 -0500

    Add files via upload

---
+++ 00-wmi-CIM-Classes.ps1
@@ -0,0 +1,15 @@
+﻿cls
+# Get-Help *-Cim* # Get
+<# 
+
+CIM Common Information Model session is a client-side object that
+represent a connection to a local computer or a remote computer
+
+#>
+
+
+Get-CimInstance -ClassName Win32_Process
+# Get-CimInstance -Query "SELECT * from Win32_Process" 
+
+
+
