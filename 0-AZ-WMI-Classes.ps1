be998642744e8a11548ee2e978975e3325471e63
zparilti <67382808+zparilti@users.noreply.github.com>
2024-11-21 10:46:16 -0500

    Add files via upload

---
+++ 0-AZ-WMI-Classes.ps1
@@ -0,0 +1,16 @@
+﻿# Get all VMs in Parallel
+$myAzureVMs ForEach-Object -Parallel {
+    
+    $out Invoke-AzVMRunCommand
+        -ResourceGroupName $_.ResourceGroupName
+        -Name $_.Name
+        -CommandId 'RunPowerShellScript'
+        -ScriptPath .\script.ps1
+
+    #Formatting the output with the VM Name
+
+
+
+
+
+}
