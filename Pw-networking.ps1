73766579be98d491d8a0f0b2ba07536cd6389d12
zparilti <67382808+zparilti@users.noreply.github.com>
2025-05-20 08:21:39 -0400

    Add files via upload

---
+++ Pw-networking.ps1
@@ -0,0 +1,14 @@
+1. Get-NetIPAddress -- view IP address settings
+2. Get-NetIPConfiguration -- display IP address details
+3. Get-NetAdapter -- display network interface settings
+4 and 5. Enable-NetAdapter and Disable-NetAdapter -- up and down an interface
+6. Get-NetRoute -- show the routing table
+7. Test-Connection and Test-NetConnection -- test connectivity
+8. Get-NetTCPConnection -- test existing connections to server
+9. ipconfig /all -- display ipaddress, subnet mask and default gateway
+10. Resolve-DnsName -- confirm name resolution works
+11. Get-DnsClient -- check DNS client status
+12 and 13. Get-DnsClientCache and Clear-DnsClientCache -- view and clear DNS cache
+14. Get-DnsClientServerAddress -- display configured DNS servers
+15. Get-Service -- check service status
+16. Restart-Service -- restart a service
